1362d9c7dca1e4fcda8aef1dfd4396f4552e9665
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-21 15:37:23 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,5 +1,11 @@
 class MusicImporter
-  def initialize
+  attr_reader :path
 
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir.glob("#{path}/*").map{ |file| file.gsub{"#{path}/", ''}}
   end
 end
