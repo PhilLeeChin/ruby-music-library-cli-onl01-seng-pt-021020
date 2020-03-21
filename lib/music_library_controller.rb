a73c447c9ef171dae31a7a3b70337d9efaacd214
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-21 16:28:20 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -25,7 +25,7 @@ class MusicLibraryController
 
   def list_songs
     Song.all.sort_by(&:name).each.with_index(1) do |song, dex|
-      puts "#{dex}.  #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      puts "#{dex}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
 
